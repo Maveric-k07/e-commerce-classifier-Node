992092afa6d756b01b01bfa41f37b9dde24e8d0d
Maveric-k07 <gurramakhileshwar333@gmail.com>
2022-07-20 17:40:42 +0530

    add procfile

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express, { urlencoded } from 'express';
 import { google } from 'googleapis';
-// const axios = require('axios');
+import axios from 'axios';
 import fetch from "node-fetch";
-import { load } from "cheerio";
-
+// import { load } from "cheerio";
+const Ecommerce = ["shopify", "woo_commerce", "wordpress", "bigcommerce", "magento"]
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -12,35 +12,90 @@ app.set("view engine", "ejs");
 app.use(urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index");
+    res.render("index");
 });
 
-const getRawData = (URL) => {
-    return fetch(URL)
-        .then((response) => response.text())
-        .then((data) => {
-            return data;
-        });
+
+// let result = '';
+
+// const getRawData = (URL) => {
+//     return fetch(URL)
+//         .then((response) => response.text())
+//         .then((data) => {
+//             return data;
+//         });
+// };
+
+// const elementIfShopify = "#bd > table > tbody > tr > td.tech_main > p:nth-child(11) > a";
+// const elementIfBigcommerce = "#bd > table > tbody > tr > td.tech_main > p:nth-child(22) > a";
+// const elementIfMagnetoAndWoo = "#bd > table > tbody > tr > td.tech_main > p:nth-child(15) > a";
+// const elementIfPageNotFound = '#bd > table > tbody > tr > td.tech_main > h1'
+// const getCategory = async(URL, element) => {
+//     const categoryRawData = await getRawData(URL);
+
+//     // parsing the data
+//     const $ = load(categoryRawData);
+//     // const element = elementIfMagnetoAndWoo;
+
+//     // const category = $(element);
+
+//     // return category.text();
+
+//     if ($(elementIfShopify).text().toLowerCase() === 'Shopify'.toLowerCase()) {
+//         return "Shopify";
+//     } else if ($(elementIfBigcommerce).text().toLowerCase() === 'Bigcommerce'.toLowerCase()) {
+//         return "Bigcommerce";
+//     } else if ($(elementIfMagnetoAndWoo).text().toLowerCase() === 'Magento'.toLowerCase() || $(elementIfMagnetoAndWoo).text().toLowerCase() === 'WooCommerce'.toLowerCase()) {
+//         return $(elementIfMagnetoAndWoo).text();
+//     } else {
+//         return "Others"
+//     }
+
+// };
+
+
+
+const getCategory = async(URL) => {
+    let data = '{"url":  "' + URL + '"}';
+
+    const options = {
+        method: 'POST',
+        url: 'https://klazify-iab-categorization.p.rapidapi.com/3rdparty/categorize',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': '21c13db4e8msha9b486c7272b1c8p110034jsn034f1d521021',
+            'X-RapidAPI-Host': 'klazify-iab-categorization.p.rapidapi.com'
+        },
+        data: data
+    };
+
+    const func = axios.request(options).then(function(response) {
+        var found = false;
+        var i, j;
+        for (i = 0; i < Ecommerce.length; i++) {
+            for (j = 0; j < response.data.objects.company.tech.length; j++) {
+                if (Ecommerce[i] === response.data.objects.company.tech[j]) {
+                    found = true
+                    break;
+                }
+            }
+            if (found) {
+                break;
+            }
+        }
+        if (found) {
+            return Ecommerce[i].toUpperCase();
+        } else {
+            return "OTHERS"
+        }
+    }).catch(function(error) {
+        return "NOT WORKING";
+    });
+    return func;
 };
 
-const elementIfShopify = "#bd > table > tbody > tr > td.tech_main > p:nth-child(11) > a";
-const elementIfBigcommerce = "#bd > table > tbody > tr > td.tech_main > p:nth-child(22) > a";
-const elementIfMagnetoAndWoo = "#bd > table > tbody > tr > td.tech_main > p:nth-child(15) > a";
 
-const getCategory = async(URL, element) => {
-    const categoryRawData = await getRawData(URL);
-
-    // parsing the data
-    const $ = load(categoryRawData);
-    // const element = elementIfMagnetoAndWoo;
-
-    const category = $(element);
-    
-    return category.text();
-
-};
-
-app.post('/', async (req, res) => {
+app.post('/', async(req, res) => {
 
     const { sheetlink } = req.body;
 
@@ -50,15 +105,15 @@ app.post('/', async (req, res) => {
         keyFile: "credentials.json",
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
-    
+
     // create client instance
     const client = await auth.getClient();
 
     //instance of sheets api
-    const googleSheets = google.sheets({version: "v4", auth: client});
+    const googleSheets = google.sheets({ version: "v4", auth: client });
 
     const spreadsheetId = capturedId[1];
-   
+
     const getRows = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
@@ -71,17 +126,16 @@ app.post('/', async (req, res) => {
     var newArray = [
         ['Categories'],
     ];
-    
-    for (var i = 1; i < n; i++){
+
+    for (var i = 1; i < n; i++) {
         var temp = array[i];
         var website = temp[0];
-        var URL = "https://w3techs.com/sites/info/";
-        var afterRegex = website.replace(/.+\/\/|www.|/g, '');
-        URL += afterRegex;
-       
-        var category = await getCategory(URL, elementIfShopify);
-        
-        newArray.push([category]);
+        // var URL = "https://w3techs.com/sites/info/";
+        // var afterRegex = website.replace(/.+\/\/|www.|shop./g, '');
+        // URL += afterRegex;
+
+        var category = await getCategory(website);
+        newArray.push([category.replace("WORDPRESS", "WOO_COMMERCE")]);
     }
 
     await googleSheets.spreadsheets.values.append({
@@ -94,7 +148,7 @@ app.post('/', async (req, res) => {
         },
     });
 
-    res.send("Websites categories!");
+    res.send("Websites categorized! Check your google sheets for the updated categories.");
 
 });
 
